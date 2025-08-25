caf83253160049ab08f0067aabc0de1d0b72f3cb
Karl Schriek <karl.schriek@bma.network>
2025-08-25 14:45:50 +0200

    add "enable_private_gateway" flag

--- main.tf
+++ main.tf
@@ -1,56 +1,56 @@
 
-
-module istio {
-  source = "github.com/schrieksoft/module-kustomization.git?ref=main"
-  absolute_paths = [
+locals {
+  absolute_paths = merge([
     "${path.root}/manifests/istio-base",
     "${path.root}/manifests/istio-discovery",
-    "${path.root}/manifests/istio-gateway",
-    "${path.root}/manifests/istio-gateway-private"
-    ]
-  patches = [
+    "${path.root}/manifests/istio-gateway"
+  ], var.enable_private_gateway ? ["${path.root}/manifests/istio-gateway-private"] : [])
+
+  patches = merge([
     {
-      patch = templatefile("${path.module}/patches/istio-base/namespace.yaml.tftpl",{
-        namespace_tolerations = jsonencode(var.istio_system_namespace_tolerations)
+      patch = templatefile("${path.module}/patches/istio-base/namespace.yaml.tftpl", {
+        namespace_tolerations   = jsonencode(var.istio_system_namespace_tolerations)
         namespace_node_selector = jsonencode(var.istio_system_namespace_node_selector)
       })
     },
 
     //discovery
     {
-      patch = templatefile("${path.module}/patches/istio-discovery/deployment.yaml.tftpl",{
-        cpu_request = jsonencode(var.istio_discovery_cpu_request)
+      patch = templatefile("${path.module}/patches/istio-discovery/deployment.yaml.tftpl", {
+        cpu_request    = jsonencode(var.istio_discovery_cpu_request)
         memory_request = jsonencode(var.istio_discovery_memory_request)
       })
     },
 
     // public gateway
     {
-      patch = templatefile("${path.module}/patches/istio-gateway/namespace.yaml.tftpl",{
-        namespace_tolerations = jsonencode(var.istio_gateway_namespace_tolerations)
+      patch = templatefile("${path.module}/patches/istio-gateway/namespace.yaml.tftpl", {
+        namespace_tolerations   = jsonencode(var.istio_gateway_namespace_tolerations)
         namespace_node_selector = jsonencode(var.istio_gateway_namespace_node_selector)
       })
     },
 
     {
-      patch = templatefile("${path.module}/patches/istio-gateway/service.yaml.tftpl",{
+      patch = templatefile("${path.module}/patches/istio-gateway/service.yaml.tftpl", {
         resource_group_name = var.resource_group_name
-        public_ip = var.public_gateway_ip
+        public_ip           = var.public_gateway_ip
       })
     },
-    
-
-    // private gateway
+    ], var.enable_private_gateway ? [ // private gateway
     {
-      patch = templatefile("${path.module}/patches/istio-gateway-private/namespace.yaml.tftpl",{
-        namespace_tolerations = jsonencode(var.istio_gateway_namespace_tolerations)
+      patch = templatefile("${path.module}/patches/istio-gateway-private/namespace.yaml.tftpl", {
+        namespace_tolerations   = jsonencode(var.istio_gateway_namespace_tolerations)
         namespace_node_selector = jsonencode(var.istio_gateway_namespace_node_selector)
       })
     },
     {
-      patch = templatefile("${path.module}/patches/istio-gateway-private/service.yaml.tftpl",{
+      patch = templatefile("${path.module}/patches/istio-gateway-private/service.yaml.tftpl", {
         private_ip = var.private_gateway_reserved_ip
       })
-    },
-  ]
+  }, ] : [])
+}
+module "istio" {
+  source         = "github.com/schrieksoft/module-kustomization.git?ref=main"
+  absolute_paths = local.absolute_paths
+  patches        = local.patches
 }
--- variables.tf
+++ variables.tf
@@ -1,19 +1,21 @@
 variable "istio_system_namespace_tolerations" {
-    default = "[]"
+  default = "[]"
 }
 variable "istio_system_namespace_node_selector" {
-    default = "priority=regular"
+  default = "priority=regular"
 }
 variable "istio_gateway_namespace_tolerations" {
-    default = "[]"
+  default = "[]"
 }
 variable "istio_gateway_namespace_node_selector" {
-    default = "priority=regular"
+  default = "priority=regular"
 }
+
+variable "enable_private_gateway" { default = false }
 variable "resource_group_name" {}
 variable "location" {}
 variable "ingress_subnet_id" {}
 variable "private_gateway_reserved_ip" {}
 variable "public_gateway_ip" {}
-variable "istio_discovery_cpu_request" { default = "250m"}
+variable "istio_discovery_cpu_request" { default = "250m" }
 variable "istio_discovery_memory_request" { default = "512Mi" }
